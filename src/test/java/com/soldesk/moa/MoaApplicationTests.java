3144efeb2f234de1f90937a76b0be5d5965c49cf
yongsky1025 <qaz7984@gmail.com>
2026-01-08 13:12:18 +0900

    project생성

---
+++ src/test/java/com/soldesk/moa/MoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soldesk.moa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
