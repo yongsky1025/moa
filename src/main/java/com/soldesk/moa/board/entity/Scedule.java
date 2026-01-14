746521ed27c9eb3643e8a412cec5d653808d0006
jae yuk <utopia2134@gmail.com>
2026-01-14 16:17:49 +0900

    1234

---
+++ src/main/java/com/soldesk/moa/board/entity/Scedule.java
@@ -0,0 +1,35 @@
+package com.soldesk.moa.board.entity;
+
+import com.soldesk.moa.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+@Table
+@Entity
+public class Scedule extends BaseEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long boardId;
+
+    @Column(unique = true)
+    private String title;
+
+    private String content;
+
+}
