82be2e2ee3aa7b06c6d2cee94d45a8cf025eb469
김건우 <kantenna11@gmail.com>
2026-03-10 13:16:59 +0900

    일정 조회, 수정 컨트롤러

--- backend/src/main/java/com/soldesk/moa/schedule/controller/ScheduleController.java
+++ backend/src/main/java/com/soldesk/moa/schedule/controller/ScheduleController.java
@@ -1,16 +1,21 @@
 package com.soldesk.moa.schedule.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.soldesk.moa.schedule.dto.ScheduleCreateRequestDTO;
 import com.soldesk.moa.schedule.dto.ScheduleResponseDTO;
+import com.soldesk.moa.schedule.dto.ScheduleUpdateRequestDTO;
 import com.soldesk.moa.schedule.service.ScheduleService;
 import com.soldesk.moa.auth.dto.AuthUserDTO;
 
