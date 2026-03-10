d47a1d53247a7c426fc086abf37cda845801bf59
umdelii <sang9205@gmail.com>
2026-03-10 14:36:45 +0900

    temp

--- backend/src/main/java/com/soldesk/moa/admin/dashboard/repository/AdminPostRepository.java
+++ backend/src/main/java/com/soldesk/moa/admin/dashboard/repository/AdminPostRepository.java
@@ -1,9 +1,13 @@
 package com.soldesk.moa.admin.dashboard.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.soldesk.moa.board.entity.Post;
 
 public interface AdminPostRepository extends JpaRepository<Post, Long>, SearchPostRepository {
 
+    // 오늘 게시글 수
+    long countTodayPosts(LocalDateTime start, LocalDateTime end);
 }
