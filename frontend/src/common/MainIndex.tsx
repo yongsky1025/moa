c3b50e5c02f14d871180ec199cf54e11fd2d0e9e
김건우 <kantenna11@gmail.com>
2026-03-10 10:53:38 +0900

    경로수정

--- frontend/src/common/MainIndex.tsx
+++ frontend/src/common/MainIndex.tsx
@@ -1,5 +1,5 @@
-import Footer from "../layout/Footer";
-import Navbar from "../layout/Navbar";
+import Footer from "./layout/Footer";
+import Navbar from "./layout/Navbar";
 
 function MainIndex() {
   return (
