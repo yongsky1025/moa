22e342fbf18061b305b5e65b859c67ff88ce9a38
umdelii <sang9205@gmail.com>
2026-03-12 13:14:45 +0900

    securityconfig admin, place, swagger 수정

--- backend/src/main/java/com/soldesk/moa/admin/dashboard/repository/SearchUsersRepositoryImpl.java
+++ backend/src/main/java/com/soldesk/moa/admin/dashboard/repository/SearchUsersRepositoryImpl.java
@@ -174,9 +174,9 @@ public class SearchUsersRepositoryImpl extends QuerydslRepositorySupport
                         }
                     }
                     break;
-                case "phone":
-                    builder.and(users.phone.contains(searchDTO.getKeyword()));
-                    break;
+                // case "phone": // 컬럼 삭제로 인해 불필요
+                // builder.and(users.phone.contains(searchDTO.getKeyword()));
+                // break;
 
             }
         }
