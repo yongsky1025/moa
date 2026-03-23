36405df0a3dee190a16faa0ecf72eec29f35eb13
김건우 <kantenna11@gmail.com>
2026-03-23 10:04:35 +0900

    태그/리뷰 조회 쿼리 추가

--- backend/src/main/java/com/soldesk/moa/place/repository/PlaceRepository.java
+++ backend/src/main/java/com/soldesk/moa/place/repository/PlaceRepository.java
@@ -1,9 +1,14 @@
 package com.soldesk.moa.place.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.soldesk.moa.place.entity.Place;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
+    @Query("SELECT DISTINCT p FROM Place p LEFT JOIN FETCH p.tags pt LEFT JOIN FETCH pt.tag LEFT JOIN FETCH p.reviews")
+    List<Place> findAllWithTagsAndReviews();
 }
